f27a367a8b99bc9b7049e224c8909f4f381fb397
Jakhongir <tashpulatov.jakhongir@gmail.com>
2022-05-07 11:07:00 +0200

    using cache

--- src/genius.js
+++ src/genius.js
@@ -1,21 +1,32 @@
 import Genius from "genius-lyrics";
 
-export async function search(q) {
+export async function search(q, mongo) {
+	console.log("start searching")
 	const Client = new Genius.Client();
 	const searches = await Client.songs.search(q);
-	return Promise.all(searches.map(async song => {
+	console.log("end searching")
+	return Promise.all(searches.map(async (song, i) => {
 		return {
 			title: `${song.artist.name} - ${song.title}`,
 			thumb_url: song.thumbnail,
 			id: song.id.toString(),
 			type: "article",
-			input_message_content: {message_text: await getLyrics(song)},
+			input_message_content: {message_text: await getLyrics(song, mongo)},
 		}
 	}));
 }
 
 
-async function getLyrics(song) {
-	let lyrics = await song.lyrics()
-	return (`${song.artist.name} - ${song.title}\n\n` + lyrics.toString().replace(/\[.*?]/g, "")).substring(0, 4093) + '...';
+async function getLyrics(song, mongo) {
+	console.log("start fetching", song.id)
+	let lyrics = await mongo.collection("lyrics").find({id: song.id}).toArray()
+	if (lyrics.length > 0) {
+		console.log("got from db", song.id)
+		return lyrics[0].lyrics
+	}
+	lyrics = await song.lyrics()
+	console.log("end fetching", song.id)
+	lyrics = (`${song.artist.name} - ${song.title}\n\n` + lyrics.toString().replace(/\[.*?]/g, "").replace("\n\n", "\n")).substring(0, 4096);
+	await mongo.collection("lyrics").insertOne({id: song.id, lyrics: lyrics})
+	return lyrics;
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import {config} from "dotenv";
 import {Telegraf} from "telegraf";
 import {search} from "./genius.js";
+import {MongoClient} from "mongodb";
 
 config();
 
 const bot = new Telegraf((process.env.BOT_TOKEN || "").toString());
-
+const client = new MongoClient((process.env.MONGO_URL || "").toString());
 process.once("SIGINT", () => bot.stop("SIGINT"));
 process.once("SIGTERM", () => bot.stop("SIGTERM"));
 
@@ -18,17 +19,20 @@ bot.on("inline_query", async (ctx) => {
 	console.log("here")
 	const query = ctx.inlineQuery?.query || "AJR";
 	try {
-		let e = await ctx.answerInlineQuery(await search(query));
+		let e = await ctx.answerInlineQuery(await search(query, ctx.mongodb));
 		console.log(e)
 	} catch (e) {
 	}
 })
 
-
-bot.launch({
-	webhook: {
-		domain: process.env.WEBHOOK, port: process.env.PORT, path: "/webhook"
+const options = {}
+if (process.env.MODE === "production") {
+	options.webhook = {
+		domain: process.env.WEBHOOK, port: process.env.PORT
 	}
-}).then(() => {
+}
+bot.launch(options).then(async () => {
+	await client.connect();
+	bot.context.mongodb = client.db("lyricfindbot");
 	console.log("Bot started");
 });
