4499b354b2184230f5e9576bcb8ed0431e3d1f70
Jakhongir <tashpulatov.jakhongir@gmail.com>
2022-05-09 12:31:46 +0200

    added private message support

--- src/genius.js
+++ src/genius.js
@@ -1,31 +1,40 @@
 import Genius from "genius-lyrics";
 
-export async function search(q, mongo) {
-	console.log("start searching")
-	const Client = new Genius.Client();
-	const searches = await Client.songs.search(q);
-	console.log("end searching")
-	return Promise.all(searches.map(async (song, i) => {
-		return {
-			title: `${song.artist.name} - ${song.title}`,
-			thumb_url: song.thumbnail,
-			id: song.id.toString(),
-			type: "article",
-			input_message_content: {message_text: await getLyrics(song, mongo)},
-		}
-	}));
+export async function search(q, mongo, lyrics = true) {
+	try {
+		console.log("start searching")
+		const Client = new Genius.Client();
+		const searches = await Client.songs.search(q);
+		console.log("end searching")
+		return Promise.all(searches.map(async (song, i) => {
+			return {
+				title: `${song.artist.name} - ${song.title}`,
+				thumb_url: song.thumbnail,
+				id: song.id.toString(),
+				type: "article",
+				input_message_content: {message_text: lyrics ? await getLyrics(song, mongo) : "No lyrics found"},
+			}
+		}));
+
+	} catch (e) {
+		return []
+	}
+}
+
+export async function getSong(id) {
+	const Client = new Genius.Client("mql0WC9UpE8kttr0wyNSnUK_JGnS5lWxylxAPMXP-UdM7QAx3yQa_RtKimW67rFV");
+	return await Client.songs.get(parseInt(id));
 }
 
 
-async function getLyrics(song, mongo) {
-	console.log("start fetching", song.id)
+export async function getLyrics(song, mongo) {
 	let lyrics = await mongo.collection("lyrics").find({id: song.id}).toArray()
 	if (lyrics.length > 0) {
 		console.log("got from db", song.id)
 		return lyrics[0].lyrics
 	}
+	song = await getSong(song.id)
 	lyrics = await song.lyrics()
-	console.log("end fetching", song.id)
 	lyrics = (`${song.artist.name} - ${song.title}\n\n` + lyrics.toString().replace(/\[.*?]/g, "").replace("\n\n", "\n")).substring(0, 4096);
 	await mongo.collection("lyrics").insertOne({id: song.id, lyrics: lyrics})
 	return lyrics;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import {config} from "dotenv";
 import {Telegraf} from "telegraf";
-import {search} from "./genius.js";
+import {getLyrics, search} from "./genius.js";
 import {MongoClient} from "mongodb";
 
 config();
@@ -10,10 +10,53 @@ const client = new MongoClient((process.env.MONGO_URL || "").toString());
 process.once("SIGINT", () => bot.stop("SIGINT"));
 process.once("SIGTERM", () => bot.stop("SIGTERM"));
 
+bot.use((ctx, next) => {
+	try {
+		ctx.mongodb.collection("users").updateOne({id: ctx.from.id}, {
+			$set: {
+				id: ctx.from.id, username: ctx.from.username, name: ctx.from.first_name + (ctx.from.last_name)
+			}
+		}, {upsert: true});
+	} catch (e) {
+		console.error(e);
+	}
+	return next();
+})
+
 bot.catch((err) => {
 	console.error(err);
 })
 
+bot.command("start", (ctx) => {
+	ctx.reply("Hello, I'm @LyricFindBot. I'm a bot that will search for lyrics for you.\n\n" + "Send me a song title and I'll search for it.\n\n" + "Did you know that I can also search for lyrics outside this chat? Just type @LyricFindBot in another chat and " + "I'll search for it there.");
+})
+
+bot.on("message", async (ctx) => {
+	if (ctx.chat.type === "private") {
+		let message = await ctx.reply("Searching for lyrics...");
+		if (message.length > 50) {
+			return await ctx.reply("Sorry, Search text is too long");
+		}
+		console.log(message)
+		const lyrics = await search(ctx.message.text, ctx.mongodb, false);
+		await ctx.telegram.deleteMessage(ctx.chat.id, message.message_id);
+		if (lyrics.length === 0) {
+			ctx.reply("I couldn't find any lyrics for that song.");
+		} else {
+			ctx.reply("*Results:*\n\n", {
+				parse_mode: "markdown",
+				reply_markup: {inline_keyboard: lyrics.map((lyric) => [{text: lyric.title, callback_data: lyric.id}])}
+			});
+		}
+	}
+})
+
+bot.on("callback_query", async (ctx) => {
+	const lyrics = await getLyrics({id: ctx.update.callback_query.data}, ctx.mongodb);
+	console.log(lyrics)
+	ctx.reply(lyrics);
+	ctx.answerCbQuery()
+})
 
 bot.on("inline_query", async (ctx) => {
 	console.log("here")
